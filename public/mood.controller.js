95a9a043741aae69806fbb2983d0af14ae0bd562
Sam Peppard <sam@o3admins-MacBook-Pro-2.local>
2017-01-20 12:57:25 -0500

    take out moodLight req variables

--- public/mood.controller.js
+++ public/mood.controller.js
@@ -12,9 +12,6 @@ angular.module('mood.controller', [])
 
         $scope.moodHue = moodService.determineMoodHue(response.data);
 
-        $scope.moodLightReqBody = $scope.moodHue;
-        $scope.moodLightReqBody.twitter_handle = $scope.twitter;
-
         $scope.conscientiousness = moodService.getConscientiousnessPercentage($scope.array);
         $scope.openness = moodService.getOpennessPercentage($scope.array);
         $scope.agreeableness = moodService.getAgreeablenessPercentage($scope.array);
@@ -24,7 +21,7 @@ angular.module('mood.controller', [])
         return $http({
           'method': 'POST',
           'url': '/api/watson/setMoodLight',
-          'data': $scope.moodLightReqBody
+          'data': $scope.moodHue
         });
       }, err => {
         console.log(err);
