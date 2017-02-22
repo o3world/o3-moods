ca27d05465db3972e098fdf68cfc5cfbaec1d706
Sam Peppard <sam@o3admins-MacBook-Pro-2.local>
2017-02-21 19:51:50 -0500

    add angular and scss updates

--- public/mood.controller.js
+++ public/mood.controller.js
@@ -43,15 +43,30 @@ angular.module('mood.controller', [])
     $scope.appStart = true;
     $scope.twitterHandleSubmitted = false;
     $scope.twitter = '';
+    $scope.moodHue.red = 255;
+    $scope.moodHue.green = 255;
+    $scope.moodHue.blue = 255;
   };
 
   function twitterHandleSubmit() {
+    $scope.appStart = false;
     $scope.twitterHandleSubmitted = true;
   }
 
 
 
   function determineColor() {
+
+    if ($scope.moodHue.red <= 200 && $scope.moodHue.red >= 61 && $scope.moodHue.green >= 43 && $scope.moodHue.green <= 192 && $scope.moodHue.blue <= 145) {
+      $scope.colorResult = 'BROWN';
+      $scope.colorDescription = 'Brown generally means the person is feeling jittery and the mind is wandering.';
+    }
+
+    if ($scope.moodHue.red >= 228 && $scope.moodHue.blue >= 208 && $scope.moodHue.blue >= 10 && $scope.moodHue.blue <= 200) {
+      $scope.colorResult = 'YELLOW';
+      $scope.colorDescription = 'The shades of yellow show a range of emotions including imaginative, confused, upset, anxious, feeling poetic and deeply observing.';
+    }
+
     if ($scope.moodHue.red >= 112 && $scope.moodHue.green <= 194 && $scope.moodHue.blue <= 194) {
       $scope.colorResult = 'RED';
       $scope.colorDescription = 'Generally associated with emotional state of passion.Shows excitement, energy and adventure. At one end of deep red, it shows arousal, passion and love. Bright red shows anger, feelings of adventure, excitement or terror. Orange is a dare devilish feeling, stress and confusion. Dark orange is worry, aggression or tensed.';
@@ -70,21 +85,11 @@ angular.module('mood.controller', [])
     if ($scope.moodHue.red <= 204 && $scope.moodHue.green <= 204 && $scope.moodHue.blue >= 102) {
       $scope.colorResult = 'BLUE';
       $scope.colorDescription = 'Known as the color of optimism andjoy, the ring will mean happiness, at peace. Blue can mean deep thinking, feeling flirty, intense moods, calm, love, passionate and romantic feeling.';
-    }
 
-    if ($scope.moodHue.red <= 200 && $scope.moodHue.red >= 61 && $scope.moodHue.green >= 43 && $scope.moodHue.green <= 192 && $scope.moodHue.blue <= 145) {
-      $scope.colorResult = 'BROWN';
-      $scope.colorDescription = 'Brown generally means the person is feeling jittery and the mind is wandering.';
-    }
-
-    if ($scope.moodHue.red <= 191 && $scope.moodHue.red >= 105 && $scope.moodHue.green <= 148 && $scope.moodHue.blue <= 255 && $scope.moodHue.blue >= 150) {
-      $scope.colorResult = 'PURPLE';
-      $scope.colorDescription = 'Purple reflects tranquil, satisfied, balanced inside, passionate, sensual and romantic. Reddish purple indicates anger, moody or desperate.';
-    }
-
-    if ($scope.moodHue.red >= 228 && $scope.moodHue.blue >= 208 && $scope.moodHue.blue >= 10 && $scope.moodHue.blue <= 200) {
-      $scope.colorResult = 'YELLOW';
-      $scope.colorDescription = 'The shades of yellow show a range of emotions including imaginative, confused, upset, anxious, feeling poetic and deeply observing.';
+      if ($scope.moodHue.red <= 200 && $scope.moodHue.red >= 105 && $scope.moodHue.green <= 148 && $scope.moodHue.blue <= 255 && $scope.moodHue.blue >= 150) {
+        $scope.colorResult = 'PURPLE';
+        $scope.colorDescription = 'Purple reflects tranquil, satisfied, balanced inside, passionate, sensual and romantic. Reddish purple indicates anger, moody or desperate.';
+      }
     }
   }
 }]);
